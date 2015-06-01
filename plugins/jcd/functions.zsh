d005694c556d40b2164b7fa218e754f9cb06ad63
JC Delay <delay@adacore.com>
2015-06-01 11:56:40 +0200

    Augment ^z

--- plugins/jcd/functions.zsh
+++ plugins/jcd/functions.zsh
@@ -23,3 +23,16 @@ function ip-addr() {
 function zsh-time() {
     time zsh -i -c exit
 }
+
+# Use ^Z to pause and resume processes
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
