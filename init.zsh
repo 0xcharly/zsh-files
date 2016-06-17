8c61366d306eb0c0dd6ad4fed43c91b67d9728ac
Charly Delay <delay@adacore.com>
2016-06-17 14:57:29 -0400

    Rename jcd -> delay

--- init.zsh
+++ init.zsh
@@ -7,13 +7,13 @@ source $SOURCE/plugins/cygwin/cygwin.plugin.zsh
 
 source $SOURCE/plugins/gerrit/gerrit.plugin.zsh
 
-source $SOURCE/plugins/jcd/aliases.zsh
-source $SOURCE/plugins/jcd/exports.zsh
-source $SOURCE/plugins/jcd/functions.zsh
-source $SOURCE/plugins/jcd/history.zsh
-source $SOURCE/plugins/jcd/grep.zsh
-source $SOURCE/plugins/jcd/options.zsh
-source $SOURCE/plugins/jcd/spectrum.zsh
+source $SOURCE/plugins/delay/aliases.zsh
+source $SOURCE/plugins/delay/exports.zsh
+source $SOURCE/plugins/delay/functions.zsh
+source $SOURCE/plugins/delay/history.zsh
+source $SOURCE/plugins/delay/grep.zsh
+source $SOURCE/plugins/delay/options.zsh
+source $SOURCE/plugins/delay/spectrum.zsh
 
 if [ -f "$HOME/.customrc-$SHORT_HOST" ]; then
     source "$HOME/.customrc-$SHORT_HOST"
