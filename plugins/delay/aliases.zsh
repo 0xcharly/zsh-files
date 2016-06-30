03584a278cbba8ec49eb5a29abfb97ea2e565ae5
Charly Delay <delay@adacore.com>
2016-06-30 16:47:22 -0400

    chore(aliases): augment bt-mail shell function

--- plugins/delay/aliases.zsh
+++ plugins/delay/aliases.zsh
@@ -1,14 +1,10 @@
 alias ssh='TERM=xterm-256color ssh'
 alias ssr='ssh -l root'
 alias ssg='ssh -l gnatmail'
-alias ck='ssh ssh.gnat.com'
+alias ct='ssh ssh.gnat.com'
 alias cx='ssh xen2.gnat.com'
 alias ch='ssh ssh.eu.adacore.com'
 alias cm='ssh imap-eu.adacore.com'
-
-alias cl='cless'
-alias ,l='|less'
-alias k='k -h'
 
 alias tmux='tmux -2'
 alias tree='tree -C'
@@ -23,18 +19,28 @@ function bt-mail {
         return 1
     fi
 
+    MUTT_FLAGS=""
+    MUTT_FLAGS="$MUTT_FLAGS -e 'color index white default \"~O\"'"
+    MUTT_FLAGS="$MUTT_FLAGS -e 'color index white default \"~N\"'"
     tn=$1; shift
-    mutt -e 'set index_format = "index-fmt-mutt %[%s] |"' \
-        -e 'color index white default "~O"' \
-        -e 'color index white default "~N"' \
-        -f /reportd/gnatbugs/${tn:0:2}/$tn/comment $@
+
+    if type mutt-index-fmt > /dev/null 2>&1; then
+        mutt \
+            -e 'color index white default "~O"' \
+            -e 'color index white default "~N"' \
+            -e 'set index_format = "mutt-index-fmt %[%s] |"' \
+            -f /reportd/gnatbugs/${tn:0:2}/$tn/comment $@
+    else
+        mutt \
+            -e 'color index white default "~O"' \
+            -e 'color index white default "~N"' \
+            -f /reportd/gnatbugs/${tn:0:2}/$tn/comment $@
+    fi
+
     return $?
 }
 
 alias bt='bugtool'
-alias follow='bugtool subscribe'
-
-alias btm='bt-mail'
 alias comment='bt-mail'
 
 # Platform-specific aliases
@@ -59,5 +65,6 @@ case `uname -s` in
     Linux|Darwin)
         alias vi='vi -T "$TERM-italic"'
         alias vim='vim -T "$TERM-italic"'
+        alias k='k -h'
         ;;
 esac
